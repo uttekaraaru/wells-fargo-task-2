826ebea9a6deca0eea866b77017628f457dc50c2
Aaradhya Uttekar <63aaradhyauttekar@gmail.com>
2026-06-15 23:17:59 +0530

    Added Portfolio entity with client and security relationships

--- src/main/java/com/wellsfargo/counselor/entity/Portfolio.java
+++ src/main/java/com/wellsfargo/counselor/entity/Portfolio.java
@@ -1,1 +1,46 @@
+package com.wellsfargo.counselor.entity;
 
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+public class Portfolio {
+
+    @Id
+    @GeneratedValue()
+    private long portfolioId;
+
+    @OneToOne
+    private Client client;
+
+    @OneToMany(mappedBy = "portfolio")
+    private List<Security> securities;
+
+    protected Portfolio() {
+    }
+
+    public Portfolio(Client client, List<Security> securities) {
+        this.client = client;
+        this.securities = securities;
+    }
+
+    public long getPortfolioId() {
+        return portfolioId;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public List<Security> getSecurities() {
+        return securities;
+    }
+
+    public void setSecurities(List<Security> securities) {
+        this.securities = securities;
+    }
+}
